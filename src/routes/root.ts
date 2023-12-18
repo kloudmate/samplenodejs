16deb504b976be92af8952f169a75dd7d614f66b
aritro66 <ghosharitro66@gmail.com>
2023-12-18 08:33:20 +0530

    todo routes added

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -7,6 +7,26 @@ import config from '../config'
 const logger = customLogger(path.basename(__filename))
 module.exports = app => {
 	//:TODO need to secure these apis too.
+	const cache = app.get('redisCache')
+	const db = app.get('db')
+	const { sequelize, Sequelize } = db
+
+	// Todo table
+	const Todo = sequelize.define('Todo', {
+		title: {
+			type: Sequelize.STRING,
+		},
+		description: {
+			type: Sequelize.TEXT,
+		},
+	});
+
+	Todo.sync().then(() => {
+		logger.info("Todo Model synced");
+	}).catch((err) => {
+		logger.error(`Error in Todo: ${err}`)
+	});
+
 	app.use(function (req, res, next) {
 		req.realmName = req.headers.realmname
 		req.redisKey = config.redis.serviceName
@@ -21,6 +41,7 @@ module.exports = app => {
 		// }
 	})
 	app.use(checkCache)
+
 	app.get('/', (req, res, next) => {
 		try {
 			req.dbData = `Hello world!`
@@ -30,4 +51,62 @@ module.exports = app => {
 			logger.error(`Error in get ${e}`)
 		}
 	})
+
+	app.post('/todo', async (req, res) => {
+		try {
+			const { title, description } = req.body;
+			const todo = await Todo.create({
+				title,
+				description,
+			});
+
+			res.json(todo);
+		} catch (error) {
+			console.error(error);
+			res.status(500).json({ error: 'Internal Server Error' });
+		}
+	});
+
+	// Route to get all TODO items
+	app.get('/todo', async (req, res) => {
+		try {
+			const key = 'todos_all';
+			const cachedData = await cache.get(key)
+			if (cachedData !== null) {
+				logger.info(`Cache Hit for ${key}`);
+				res.json(JSON.parse(cachedData));
+			} else {
+				logger.info(`Cache Miss for ${key}`)
+				const todos = await Todo.findAll();
+				cache.set(key, JSON.stringify(todos), 60);
+				res.json(todos);
+			}
+		} catch (error) {
+			console.error(error);
+			res.status(500).json({ error: 'Internal Server Error' });
+		}
+	});
+
+	// Route to get Todo by Id
+	app.get('/todo/:id', async (req, res) => {
+		try {
+			const { id } = req.params;
+			const key = `todos_${id}`
+			const cachedData = await cache.get(key)
+			if (cachedData !== null) {
+				logger.info(`Cache Hit for ${key}`);
+				res.json(JSON.parse(cachedData));
+			} else {
+				logger.info(`Cache Miss for ${key}`)
+				const todo = await Todo.findByPk(id);
+				cache.set(key, JSON.stringify(todo), 60);
+				res.json(todo);
+			}
+
+		} catch (error) {
+			console.error(error);
+			res.status(500).json({ error: 'Internal Server Error' });
+		}
+	});
+
 }
